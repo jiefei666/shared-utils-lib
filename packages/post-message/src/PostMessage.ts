a3382c17797f5a18d7f6f1cd90a172206b9bda63
gongcheng <525298429@qq.com>
2023-12-26 15:15:16 +0800

    feat: ts类型优化

--- packages/post-message/src/PostMessage.ts
+++ packages/post-message/src/PostMessage.ts
@@ -1,29 +1,31 @@
-import { warn, isThenable } from './util'
-import Emitter from './Emitter'
+import { warn, isThenable } from './util';
+import Emitter from './Emitter';
 
 export interface PostMessageOptions {
-  name?: string // 当前窗口名字
-  filterSources?: string[] // 和name搭配使用，过滤消息来源
+  name?: string; // 当前窗口名字
+  filterSources?: string[]; // 和name搭配使用，过滤消息来源
 }
 
 type MessageListener = (event: {
-  data: { source: PostMessageOptions['name']; type: string; data: unknown }
-}) => void
+  data: { source: PostMessageOptions['name']; type: string; data: unknown };
+}) => void;
+
+type GlobalWindow = typeof globalThis;
 
 export default class PostMessage extends Emitter {
-  target
-  options
-  exposes: Set<string>
+  target;
+  options;
+  exposes: Set<string>;
 
-  private messageListener: MessageListener
+  private messageListener: MessageListener;
 
-  constructor(target: Window, options?: PostMessageOptions) {
-    super()
-    this.target = target
-    this.options = options || {}
+  constructor(target: GlobalWindow, options?: PostMessageOptions) {
+    super();
+    this.target = target;
+    this.options = options || {};
 
     // 保存其他window暴露的方法名
-    this.exposes = new Set()
+    this.exposes = new Set();
 
     // 监听其他window调用expose方法暴露相关的方法给自己
     this.on('onAddExpose', (keys: string[]) => {
@@ -31,17 +33,17 @@ export default class PostMessage extends Emitter {
         // 方法名称重复则覆盖给出提示
         if (this.exposes.has(key)) {
           warn(
-            `The method: ${key} already exists. Please modify the name, otherwise it will be overwritten`
-          )
+            `The method: ${key} already exists. Please modify the name, otherwise it will be overwritten`,
+          );
         }
-        this.exposes.add(key)
-      })
-    })
+        this.exposes.add(key);
+      });
+    });
 
     this.messageListener = (event) => {
-      const message = event.data
+      const message = event.data;
       if (!message) {
-        return
+        return;
       }
 
       if (
@@ -51,30 +53,30 @@ export default class PostMessage extends Emitter {
           message.source &&
           this.options.filterSources.includes(message.source))
       ) {
-        console.log(`message`, message, this._events, this.exposes)
-        const { type, data } = message
-        this.trigger(type, data)
+        console.log(`message`, message, this._events, this.exposes);
+        const { type, data } = message;
+        this.trigger(type, data);
       }
-    }
+    };
 
     // 统一监听
-    target.addEventListener('message', this.messageListener, false)
+    target.addEventListener('message', this.messageListener, false);
   }
 
   removeAllListeners() {
-    this.offAll()
-    this.target.removeEventListener('message', this.messageListener)
+    this.offAll();
+    this.target.removeEventListener('message', this.messageListener);
   }
 
   // 事件通知
-  send(type: string, data: unknown, target: Window) {
-    const _target = target || this.target
+  send(type: string, data: unknown, target: GlobalWindow) {
+    const _target = target || this.target;
     const message = {
       type,
       data,
       source: this.options.name,
-    }
-    _target.postMessage(message, '*')
+    };
+    _target.postMessage(message, '*');
   }
 
   /**
@@ -82,36 +84,36 @@ export default class PostMessage extends Emitter {
    * @param {object} obj 暴露的方法或者变量
    * @param {Window} target 暴露给那个window
    */
-  expose(obj: Record<string, Function>, target: Window) {
+  expose(obj: Record<string, Function>, target: GlobalWindow) {
     if (toString.call(obj) !== '[object Object]') {
-      console.log('expose accept a object')
-      return
+      console.log('expose accept a object');
+      return;
     }
-    const keys = Object.keys(obj)
-    this.send('onAddExpose', keys, target)
+    const keys = Object.keys(obj);
+    this.send('onAddExpose', keys, target);
     keys.forEach((name) => {
       // 同一个类型的方法只能有一个回调
-      this.off(name)
+      this.off(name);
       this.on(name, async (param: unknown) => {
-        const value = obj[name]
-        let data
+        const value = obj[name];
+        let data;
         if (typeof value === 'function') {
-          const res = value(param)
+          const res = value(param);
           if (isThenable(res)) {
-            data = await res
+            data = await res;
           } else {
-            data = res
+            data = res;
           }
         } else {
-          data = value
+          data = value;
         }
-        this.send(name, data, target)
-      })
-    })
+        this.send(name, data, target);
+      });
+    });
   }
   // 判断是否暴露了方法
   hasExpose(name: string) {
-    return this.exposes.has(name)
+    return this.exposes.has(name);
   }
   /**
    * 访问iframe提供方法和变量
@@ -120,18 +122,18 @@ export default class PostMessage extends Emitter {
    * @param {Window} target 访问用那个
    * @returns
    */
-  invoke(name: string, param: unknown, target: Window) {
+  invoke(name: string, param: unknown, target: GlobalWindow) {
     if (!this.hasExpose(name)) {
       // warn(`not found invoke: ${name}`)
-      return
+      return;
     }
     return new Promise((resolve) => {
       // 同一个类型的方法只能有一个回调
-      this.off(name)
+      this.off(name);
       this.on(name, (data: unknown) => {
-        resolve(data)
-      })
-      this.send(name, param, target)
-    })
+        resolve(data);
+      });
+      this.send(name, param, target);
+    });
   }
 }
